77a53233118405be68d1239d3351908490c2930f
Patrick McCartney <mccartney.patrick@gmail.com>
2019-04-11 12:13:58 -0400

    Added job/build info

--- src/test/java/BrowserTester.java
+++ src/test/java/BrowserTester.java
@@ -20,7 +20,7 @@ public class BrowserTester {
     RemoteWebDriver driver;
     Eyes eyes;
     VisualGridRunner runner;
-    static BatchInfo batch = new BatchInfo("Build #" + System.getProperty("BuildName"));
+    static BatchInfo batch = new BatchInfo("Job:" + System.getProperty("JobName")+ " | Build #" + System.getProperty("BuildNumber"));
 
     String[][] urls = {
            // {"Capital One - Homepage", "https://www.capitalone.com", ""},
