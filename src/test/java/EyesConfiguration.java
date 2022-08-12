e3a693b6d05c6ce1190b97ad4ce69cda07a6d139
Chris Emerson <cme210@yahoo.com>
2022-08-12 09:17:11 -0400

    misc

--- src/test/java/EyesConfiguration.java
+++ src/test/java/EyesConfiguration.java
@@ -40,7 +40,6 @@ public class EyesConfiguration {
                 .setTestName(testName)
                 .setAppName(util.getParam("AppName"))
                 .setApiKey(util.getParam("APIKey"))
-                .setServerUrl(util.getParam("EyesServerUrl"))
                 .setBatch(batch)
                 .setHideScrollbars(true)
                 .setForceFullPageScreenshot(true)
